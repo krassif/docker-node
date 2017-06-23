5c2ecd4b991debd39cd1add7ecc87bedd4a53aa6
Peerbelt <build@peerbelt.com>
2017-06-23 19:27:19 -0400

    FEAT: Configurable package manager

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -3,31 +3,35 @@
 # Get running container's IP
 HOST=`hostname --ip-address`
 
+if [ -z "$MANAGER" ]; then
+    MANAGER="yarn"
+fi;
+
 # The filder containing the dependencies 
 MODULED="./node_modules";
 
 # Install depdendencies if not done already
 if [ -d "$MODULED" ]
 then
-    echo "INFO: $MODULED does exist. Skipping yarn install/"
+    echo "INFO: $MODULED does exist. Skipping $MANAGER install/"
 else
-    eval "HOST=$HOST yarn install $INSTALL_OPTS" || rm -Rf $MODULED || exit 1
+    eval "HOST=$HOST $MANAGER install $INSTALL_OPTS" || rm -Rf $MODULED || exit 1
 fi;
 
 # Run the dist script, if not done already
 if [ -e "$MODULED/dist.lock" ]
 then
-    echo "INFO: Skipping yarn dist/"
+    echo "INFO: Skipping $MANAGER dist/"
 else
-    eval "HOST=$HOST yarn run dist $DIST_OPTS" && touch "$MODULED/dist.lock"
+    eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" && touch "$MODULED/dist.lock"
 fi;
 
 # Run the tests, if not done already
 if [ -e "$MODULED/test.lock" ]
 then
-    echo "INFO: Skipping yarn test/"
+    echo "INFO: Skipping $MANAGER test/"
 else
-    eval "HOST=$HOST yarn run test $TEST_OPTS" && touch "$MODULED/test.lock"
+    eval "HOST=$HOST $MANAGER run test $TEST_OPTS" && touch "$MODULED/test.lock"
 fi;
 
 # Finally, run the start option
