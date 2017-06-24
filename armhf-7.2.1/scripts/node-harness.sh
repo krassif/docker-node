cb4302605dbc89ecbe059383fda42fb121c9583b
Peerbelt <build@peerbelt.com>
2017-06-23 20:40:34 -0400

    no message

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -13,16 +13,16 @@ MODULED="$BIN/node_modules"
 MODULEDDEST="./node_modules"
 
 # Install depdendencies if not done already
-if [[ -z "$NO_INSTALL" && -d "$MODULED" && ! -e "$MODULEDDEST/modules.lock" ]]
+if [[ ! -z "$NO_INSTALL" && -d "$MODULED" && -e "$MODULEDDEST/modules.lock" ]]
 then
     echo "INFO: $MODULED does exist. Skipping $MANAGER install/"
 else
-    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \".$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf ".$MODULEDDEST" || exit 1 ) )
+    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \".$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf ".$MODULEDDEST/*" || exit 1 ) )
     touch "$MODULEDDEST/modules.lock"
 fi;
 
 # Run the dist script, if not done already
-if [[ -z "$NO_DIST" && ! -e "$MODULEDDEST/dist.lock" ]]
+if [[ ! -z "$NO_DIST" && -e "$MODULEDDEST/dist.lock" ]]
 then
     echo "INFO: Skipping $MANAGER dist/"
 else
@@ -30,7 +30,7 @@ else
 fi;
 
 # Run the tests, if not done already
-if [[ -z "$NO_TEST" && -e "$MODULEDDEST/test.lock" ]]
+if [[ ! -z "$NO_TEST" && -e "$MODULEDDEST/test.lock" ]]
 then
     echo "INFO: Skipping $MANAGER test/"
 else
