a7e35460b7ff6f172f840c751ca511a1f52d5986
Peerbelt <build@peerbelt.com>
2017-06-23 20:34:21 -0400

    FIX: Context and folder names

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -17,7 +17,7 @@ if [[ -z "$NO_INSTALL" && -d "$MODULED" && ! -e "$MODULEDDEST/modules.lock" ]]
 then
     echo "INFO: $MODULED does exist. Skipping $MANAGER install/"
 else
-    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \"$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf $MODULED || exit 1 ) )
+    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \".$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf ".$MODULEDDEST" || exit 1 ) )
     touch "$MODULEDDEST/modules.lock"
 fi;
 
@@ -26,7 +26,7 @@ if [[ -z "$NO_DIST" && ! -e "$MODULEDDEST/dist.lock" ]]
 then
     echo "INFO: Skipping $MANAGER dist/"
 else
-    ( cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" && touch "$MODULEDDEST/dist.lock" )
+    ( cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" ) && touch "$MODULEDDEST/dist.lock"
 fi;
 
 # Run the tests, if not done already
@@ -34,7 +34,7 @@ if [[ -z "$NO_TEST" && -e "$MODULEDDEST/test.lock" ]]
 then
     echo "INFO: Skipping $MANAGER test/"
 else
-    ( cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" && touch "$MODULEDDEST/test.lock" )
+    ( cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" ) && touch "$MODULEDDEST/test.lock"
 fi;
 
 # Finally, run the start option
