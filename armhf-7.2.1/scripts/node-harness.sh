07a1ff0d453fd0d36efb87ff09f90d19ab33a815
Peerbelt <build@peerbelt.com>
2017-06-23 20:19:32 -0400

    FEAT: Working within ./bin with modules output in ./node_modules

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -7,35 +7,38 @@ if [ -z "$MANAGER" ]; then
     MANAGER="yarn"
 fi;
 
-# The filder containing the dependencies 
-MODULED="./node_modules";
+# The filder containing the dependencies
+BIN="./bin" 
+MODULED="$BIN/node_modules"
+MODULEDDEST="./node_modules"
 
 # Install depdendencies if not done already
-if [ -d "$MODULED" ]
+if [[ -z "$NO_INSTALL" && -d "$MODULED" && ! -e "$MODULEDDEST/modules.lock" ]]
 then
     echo "INFO: $MODULED does exist. Skipping $MANAGER install/"
 else
-    eval "HOST=$HOST $MANAGER install $INSTALL_OPTS" || rm -Rf $MODULED || exit 1
+    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \"$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf $MODULED || exit 1 ) )
+    touch "$MODULEDDEST/modules.lock"
 fi;
 
 # Run the dist script, if not done already
-if [ -e "$MODULED/dist.lock" ]
+if [[ -z "$NO_DIST" && ! -e "$MODULEDDEST/dist.lock" ]]
 then
     echo "INFO: Skipping $MANAGER dist/"
 else
-    eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" && touch "$MODULED/dist.lock"
+    ( cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" && touch "$MODULEDDEST/dist.lock" )
 fi;
 
 # Run the tests, if not done already
-if [ -e "$MODULED/test.lock" ]
+if [[ -z "$NO_TEST" && -e "$MODULEDDEST/test.lock" ]]
 then
     echo "INFO: Skipping $MANAGER test/"
 else
-    eval "HOST=$HOST $MANAGER run test $TEST_OPTS" && touch "$MODULED/test.lock"
+    ( cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" && touch "$MODULEDDEST/test.lock" )
 fi;
 
 # Finally, run the start option
-eval "HOST=$HOST node $NODE_OPTS" || exit 1
+( cd $BIN && eval "HOST=$HOST node $NODE_OPTS" || exit 1 )
 
 # return normal exit
 exit 0
