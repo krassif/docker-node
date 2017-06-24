f2b1acb91013d3212bbc012c0257a95b2ae6b377
Peerbelt <build@peerbelt.com>
2017-06-24 00:03:31 -0400

    FEAT: Reusable yarn cache

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -17,6 +17,7 @@ if [[ ! -z "$NO_INSTALL" || -d "$MODULED" || -e "$MODULEDDEST/modules.lock" ]]
 then
     echo "INFO: $MODULED does exist. Skipping $MANAGER install/"
 else
+    yarn config set cache-folder "~/yarn"
     ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \".$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf ".$MODULEDDEST/*" || exit 1 ) ) || exit 1
     touch "$MODULEDDEST/modules.lock"
     # chown -R root:root "$MODULEDDEST/"
