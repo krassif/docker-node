8a2f361c77d670f6c846ee74533c4766d3a45b6d
Peerbelt <build@peerbelt.com>
2017-06-29 12:54:15 -0400

    FIX: Install handling; relative folders in PATH

--- armhf-7.2.1/scripts/node-harness.sh
+++ armhf-7.2.1/scripts/node-harness.sh
@@ -14,9 +14,9 @@ if [ -z "$NODE_COMMAND" ]; then
 fi;
 
 # The filder containing the dependencies
-BIN="./bin"
+BIN="$HOME/bin"
 MODULED="$BIN/node_modules"
-MODULEDDEST="./node_modules"
+MODULEDDEST="$HOME/node_modules"
 
 # Install depdendencies if not done already
 if [[ ! -z "$NO_INSTALL" || -d "$MODULED" || -e "$MODULEDDEST/modules.lock" ]]
@@ -24,9 +24,9 @@ then
     echo "INFO: $MODULED (or modules.lock) does exist. Skipping $MANAGER install/"
 else
     yarn config set cache-folder "$HOME/yarn"
-    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \".$MODULEDDEST\" $INSTALL_OPTS" || rm -Rf ".$MODULEDDEST/*" || exit 1 ) ) || exit 1
+    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \"$MODULEDDEST\" $INSTALL_OPTS" ) ) || ( rm -Rf "$MODULEDDEST/*"; exit 1 )
     touch "$MODULEDDEST/modules.lock"
-    # chown -R root:root "$MODULEDDEST/"
+    chown -R root:root "$MODULEDDEST/"
 fi;
 
 # Run the dist script, if not done already
@@ -34,7 +34,7 @@ if [[ ! -z "$NO_DIST" || -e "$MODULEDDEST/dist.lock" ]]
 then
     echo "INFO: Skipping $MANAGER dist/"
 else
-    ( PATH="$PATH:.$MODULEDDEST/.bin:.$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" ) && touch "$MODULEDDEST/dist.lock"
+    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" ) && touch "$MODULEDDEST/dist.lock"
 fi;
 
 # Run the tests, if not done already
@@ -42,11 +42,11 @@ if [[ ! -z "$NO_TEST" || -e "$MODULEDDEST/test.lock" ]]
 then
     echo "INFO: Skipping $MANAGER test/"
 else
-    ( PATH="$PATH:.$MODULEDDEST/.bin:.$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" ) && touch "$MODULEDDEST/test.lock"
+    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" ) && touch "$MODULEDDEST/test.lock"
 fi;
 
 # Finally, run the start option
-( cd $BIN && eval "HOST=$HOST $MANAGER run $NODE_COMMAND -- $NODE_OPTS" || exit 1 )
+( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; ( cd $BIN && eval "HOST=$HOST $MANAGER run $NODE_COMMAND -- $NODE_OPTS" ) || exit 1 )
 
 # return normal exit
 exit 0
