6ab5572573935493f505dea2f5cdb0ae35492ef2
Peerbelt <build@peerbelt.com>
2017-07-13 17:15:14 -0400

    no message

--- arm64-7.2.1/scripts/node-harness.sh
+++ arm64-7.2.1/scripts/node-harness.sh
@@ -18,7 +18,25 @@ BIN="$HOME/bin"
 MODULED="$BIN/node_modules"
 MODULEDDEST="$HOME/node_modules"
 
+function install_done {
+    rm -Rf "$HOME/install.lock"
+}
+
+function finalize {
+    install_done
+    exit $1
+}
+
+function lock {
+    touch "$MODULEDDEST/$1"
+    touch "$MODULED/$1" || echo "WARNING: Failed to create $MODULED/$1"
+}
+
+# Tell the health check we are still installing
+touch "$HOME/install.lock"
+
 # Install depdendencies if not done already
+function module {
 if [[ ! -z "$NO_INSTALL" || \
     ( -d "$MODULED" && ! -L "$MODULED" ) || \
     ( -d "$MODULED" &&   -L "$MODULED" && -e "$MODULED/modules.lock" ) || \
@@ -27,30 +45,54 @@ then
     echo "INFO: $MODULED (or modules.lock) does exist. Skipping $MANAGER install/"
 else
     yarn config set cache-folder "$HOME/yarn"
-    ( cd $BIN && ( eval "HOST=$HOST $MANAGER install --no-lockfile --modules-folder \"$MODULEDDEST\" $INSTALL_OPTS" ) ) || ( rm -Rf "$MODULEDDEST/*"; exit 1 )
-    touch "$MODULEDDEST/modules.lock" 
-    touch "$MODULED/modules.lock" || echo "WARNING: Failed to create $MODULED/modules.lock"
+    ( cd $BIN && ( eval "PATH=$PATH HOST=$HOST $MANAGER install --no-lockfile --modules-folder \"$MODULEDDEST\" $INSTALL_OPTS" ) ) || ( rm -Rf "$MODULEDDEST/*"; finalize 1 )
+    lock "modules.lock"
     # chown -R root:root "$MODULEDDEST/"
 fi;
+}
 
 # Run the dist script, if not done already
+function dist {
+local UNLINK=0
 if [[ ! -z "$NO_DIST" || -e "$MODULEDDEST/dist.lock" ]]
 then
     echo "INFO: Skipping $MANAGER dist/"
 else
-    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run dist $DIST_OPTS" ) && touch "$MODULEDDEST/dist.lock"
+    if [[ ! -d "$MODULED" || ! -L "$MODULED" ]]
+    then
+        echo "Attempting to create node_modules in bin (grunt can't find tasks otherwise).."
+        ( ln -fsn "$MODULEDDEST" "$MODULED" ) || echo "WARNING: Failed mapping $MODULEDDEST to $MODULED"
+        UNLINK=1
+    fi
+    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "PATH=$PATH HOST=$HOST $MANAGER run dist $DIST_OPTS" && lock "dist.lock" ) || finalize 1
+    if (( UNLINK == 1 ))
+    then
+        unlink "$MODULED" || echo "WARNING: Failed unlinking $MODULED from $MODULEDDEST"
+    fi;
 fi;
+}
 
 # Run the tests, if not done already
+function tests {
 if [[ ! -z "$NO_TEST" || -e "$MODULEDDEST/test.lock" ]]
 then
     echo "INFO: Skipping $MANAGER test/"
 else
-    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "HOST=$HOST $MANAGER run test $TEST_OPTS" ) && touch "$MODULEDDEST/test.lock"
+    ( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; cd $BIN && eval "PATH=$PATH HOST=$HOST $MANAGER run test $TEST_OPTS"  && lock "test.lock" ) || finalize 1
 fi;
+}
+
+# Execute the prerun steps logged
+LOG="$HOME/logs/$HOSTNAME-harness.log"
+module 2>&1 | tee -a $LOG
+dist   2>&1 | tee -a $LOG
+tests  2>&1 | tee -a $LOG
+
+# remove the lock
+install_done
 
 # Finally, run the start option
-( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; ( cd $BIN && eval "HOST=$HOST $MANAGER run $NODE_COMMAND -- $NODE_OPTS" ) || exit 1 )
+( PATH="$PATH:$MODULEDDEST/.bin:$MODULED/.bin"; ( cd $BIN && eval "PATH=$PATH HOST=$HOST $MANAGER run $NODE_COMMAND -- $NODE_OPTS" ) || exit 1 )
 
 # return normal exit
-exit 0
+finalize 0
